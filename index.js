8e0f32529c4a44dc8007f5e7c109778c569c65a6
Mathias <mathiasbergqvist88@gmail.com>
2020-10-16 13:48:33 +0200

    Setup connection to db

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+const routes = require("./routes");
+
+// Connect to the MongoDB database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("🔌 DB connected");
+
+    const app = express();
+    app.use("/api", routes);
+    app.listen(5050, () => {
+      console.log("🚀 Server has started on port 5050.");
+    });
+  })
+  .catch((err) => console.error(err));
